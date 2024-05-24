cd71a600c61b86d1549c5463528a2785ba3524df
JoseCastroEnriquez <166862616+JoseCastroEnriquez@users.noreply.github.com>
2024-05-23 19:30:28 -0500

    Add files via upload

---
+++ Aprendizaje/pro.js
@@ -0,0 +1,29 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     var enlace = document.getElementById('enlace');
+//     var solucion = document.getElementById('solucion');
+  
+//     enlace.addEventListener('click', function(event) {
+//       event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+  
+//       // Muestra el cuadro de solución
+//       solucion.style.display = 'block';
+//     });
+//   });
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var enlace = document.getElementById('enlace');
+//   var solucion = document.getElementById('solucion');
+
+//   enlace.addEventListener('click', function(event) {
+//     event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+
+//     // Alternar clases para mostrar/ocultar la solución
+//     if (solucion.classList.contains('mostrado')) {
+//       solucion.classList.remove('mostrado');
+//       solucion.classList.add('oculto');
+//     } else {
+//       solucion.classList.remove('oculto');
+//       solucion.classList.add('mostrado');
+//     }
+//   });
+// });
